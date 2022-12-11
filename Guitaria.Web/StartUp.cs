e2475241016b6e8f46404cb555b04bf3912b0332
Volen Kovachev <vkovachev04@gmail.com>
2022-12-11 21:20:33 +0200

    Fixed area tag helper for search bar

--- Guitaria.Web/StartUp.cs
+++ Guitaria.Web/StartUp.cs
@@ -49,6 +49,7 @@ public class StartUp
             // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
         }
+
         //Seed Database
         app.PrepareDatabase();
         app.InitializeDatabase();
